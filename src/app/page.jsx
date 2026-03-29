1fce74a502ae551f080273e5e594891bb2472832
namanydv <namanofficial91@gmail.com>
2026-03-29 08:57:32 +0530

    Add new pages for About, Bio, Contact, and Home; remove unused globals.css

---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <h1 className="text-6xl font-bold text-gray-800 dark:text-white">
+        Home Page 2
+      </h1>
+    </div>
+  );
+}
